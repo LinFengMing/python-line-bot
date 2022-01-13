fe294d9edc71067d68f8d1a178176f992f17d15d
jiro <jiro853141@gmail.com>
2022-01-14 00:07:01 +0800

    feat: 透過 ngrok 打通地端 Web Api 接收第一個來自 Line 的訊息

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from linebot.api import LineBotApi
+from linebot.webhook import WebhookHandler
+from fastapi.params import Header
+from starlette.requests import Request
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage
 
 app = FastAPI()
 
+load_dotenv()
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/api/line")
+async def callback(request: Request, x_line_signature: str = Header(None)):
+    body = await request.body()
+    try:
+        handler.handle(body.decode("utf-8"), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid signature. Please check your channel access token/channel secret.",
+        )
+    return "OK"
 
 
-@app.post("/items")
-async def create_item(item: Item):
-    return item
-
-
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
-
-
-@app.delete("/items/{item_id}")
-async def read_item(item_id: str):
-    return {"item_id": item_id}
+@handler.add(event=MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = TextMessage(text=f"You said: {event.message.text}")
+    line_bot_api.reply_message(event.reply_token, msg)
