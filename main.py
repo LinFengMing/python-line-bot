320e4135fff21acf485c473b64a979cdc54488c3
jiro <jiro853141@gmail.com>
2022-01-14 22:50:32 +0800

    feat: 事件-加入好友.封鎖類型

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from starlette.requests import Request
 from models.message_request import MessageRequest
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage
+from linebot.models import FollowEvent, UnfollowEvent, MessageEvent, TextMessage, TextSendMessage
 from skills import *
 from skills import skills
 
@@ -38,6 +38,27 @@ async def callback(request: Request, x_line_signature: str = Header(None)):
             detail="Invalid signature. Please check your channel access token/channel secret.",
         )
     return "OK"
+
+
+@handler.add(event=FollowEvent)
+def handle_message(event):
+    print("follow", event)
+
+    # 取得使用者個人資訊
+    profile = line_bot_api.get_profile(event.source.user_id)
+    print(profile.display_name)
+    print(profile.user_id)
+    print(profile.picture_url)
+    print(profile.status_message)
+
+    # 回傳歡迎訊息
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(f"Hi, {profile.display_name}"))
+
+
+@handler.add(event=UnfollowEvent)
+def handle_message(event):
+    print("unfollow", event)
 
 
 @handler.add(event=MessageEvent, message=TextMessage)
